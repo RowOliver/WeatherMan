4b30508bbeba49fb792c07e6bb952e3322206a40
Rowan Oliver <r.oliver.yoga@gmail.com>
2020-12-30 11:06:20 -0800

    api put in, starting rspone fucntion

--- Asset/script.js
+++ Asset/script.js
@@ -1,3 +1,68 @@
+//use LET and CONST to get practice 
+var apiKey = "b7dd65fc4ea96c384fa45ce37176529c";
 
-// try using LET concept 
-let city="";
+$(document).ready(function( ){
+
+    $("#search").on("click",function(){
+      const citySearch =   $("#citySearch").val()
+        
+        currentWeather(citySearch)
+        
+
+    });
+
+function dayRequest(){
+  // series of requests for single and five day forecast 
+
+  var city = $("#citySearch").val();
+  var currentDayURL = "api.openweathermap.org/data/2.5/weather?q=" + city +  "&appid=" + APIkey;
+  var fiveDayURL = "api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid= " + APIkey;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
+
