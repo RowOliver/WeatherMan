2478e5aef2099f2665484a04e40278e59855baaa
Rowan Oliver <r.oliver.yoga@gmail.com>
2020-12-30 11:45:09 -0800

    more functions, api work and moment added

--- Asset/script.js
+++ Asset/script.js
@@ -17,7 +17,37 @@ function dayRequest(){
   var city = $("#citySearch").val();
   var currentDayURL = "api.openweathermap.org/data/2.5/weather?q=" + city +  "&appid=" + APIkey;
   var fiveDayURL = "api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid= " + APIkey;
+
+// AJAX request, go GET 
+$.ajax({
+  url: currentDayURL,
+  method: "GET"
+
+}).then(function(response){
+  //all the material that is going to be gotten from request 
+
+  var tempCD =
+  var currentDate = moment().format("MM/DD/YYYY");
+  var iconCode = response.weather[0].icon;
+  var iconURL = "http://openweathermap.org/img/w/" + iconcode + ".png";
+
+
+
+
+
+
+
+
+  console.log(respone)
+})
+
+// UV fucntions 
+function UVindex(){
+  var uvURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + searchLat "&lon=" + searchLon + "appid=" + API key;
+
 }
+
+};
 
 
 
