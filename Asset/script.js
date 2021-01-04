55b5c29a7807ac13c10eed86fa99ca9674a5e61d
Rowan Oliver <r.oliver.yoga@gmail.com>
2021-01-03 20:20:26 -0800

    let commands

---
+++ Asset/script.js
@@ -0,0 +1,15 @@
+//create lets for all actions that are required 
+let city=" ";
+let url=" ";
+let APIkey=" ";
+let queryURL =" ";
+let newURL =" ";
+let citiesDiv = document.getElementById("searched-cities");
+let cities = [ ];
+ 
+initialize();
+listFunction();
+searchFunction();
+
+
+
