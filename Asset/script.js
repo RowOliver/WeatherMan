9b08a03630e7a0b4d2412cd2744052bee9d696ca
Rowan Oliver <r.oliver.yoga@gmail.com>
2021-01-03 20:34:11 -0800

    functions added

--- Asset/script.js
+++ Asset/script.js
@@ -11,5 +11,35 @@ initialize();
 listFunction();
 searchFunction();
 
+function initialize() {
+  let saved_cities = JSON.parse(localStorage.getItem("cities"));
+  if (saved_cities !== null){
+    cities = saved_cities
+  }
 
+  rendernewButtons();
+}
+
+function storeCities(){
+  localStorage.setItem("cities", JSON.stringify(cities));
+}
+
+function rendernewButtons(){
+  citiesDiv.innerHTML = " ";
+  if(cities == null){
+    return;
+  }
+  let newCities = [...new Set(cities)];
+  for(let i=0; i < newCities.length; i++) {
+    let cityName = newCities[i];
+
+    let newButton = document.createElement("button");
+    newButton.textContent = cityName;
+    newButton.setAttribute("class", "list-button");
+
+    citiesDiv.appendChild(newButton);;
+    listFunction();
+  }
+
+}
 
